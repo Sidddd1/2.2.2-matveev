924c0109dd84a7998b668f733f8700bbf4729085
stas <sidddd1@mail.ru>
2021-08-30 17:45:29 +0300

    finish

--- 2.2.2. spring_mvc/src/main/java/web/service/CarServiceImpl.java
+++ 2.2.2. spring_mvc/src/main/java/web/service/CarServiceImpl.java
@@ -3,22 +3,35 @@ package web.service;
 
 import org.springframework.stereotype.Service;
 import web.model.Car;
+
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class CarServiceImpl implements CarService{
 
-    final
-    private CarsBD carsBD;
+    public  List<Car> getBD () {
+        List<Car> list = new ArrayList<>();
 
-    public CarServiceImpl(CarsBD carsBD) {
-        this.carsBD = carsBD;
+        Car car1 = new Car("BMW", "200", "Black");
+        Car car2 = new Car("Audi", "100", "Красный");
+        Car car3 = new Car("Запр", "1", "Серый");
+        Car car4 = new Car("Ford", "mustang", "Green");
+        Car car5 = new Car("Suzuki", "TR2", "White");
+
+        list.add(car1);
+        list.add(car2);
+        list.add(car3);
+        list.add(car4);
+        list.add(car5);
+
+        return list;
     }
 
     @Override
     public List<Car> getSomeCars(int count) {
         if(count >= 5) {
-            return carsBD.getBD();
+            return getBD();
         }
-        return carsBD.getBD().subList(0,count);
+        return getBD().subList(0,count);
     }
 }
